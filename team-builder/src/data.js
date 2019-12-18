77fa049cce7411b45e43656f635d20df0167302b
Milo Rastgoo <hello@informagician.com>
2019-12-18 15:10:01 -0500

    data added, state defined

---
+++ team-builder/src/data.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+let team = [
+    {
+        fname: "Milo",
+        lname: "Rastgoo",
+        location: "Toronto",
+        email: "hi@informagician.com"
+    },
+    {
+        fname: "Israel",
+        lname: "Gonzalez",
+        location: "Las Vegas",
+        email: "izzy@domain.com"
+    },
+    {
+        fname: "Aaron",
+        lname: "Janovitch",
+        location: "Woodland Hills",
+        email: "aj@domain.com"
+    },
+    {
+        fname: "Gus",
+        lname: "Baden",
+        location: "city",
+        email: "gus@domain.com"
+    }
+]
+
+export default team;
