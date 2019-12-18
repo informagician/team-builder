927cfa495283501717862281f48c716af3b1d1e4
Milo Rastgoo <hello@informagician.com>
2019-12-18 15:43:55 -0500

    step 1 completed - current members are rendered

--- team-builder/src/components/List.js
+++ team-builder/src/components/List.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import team from '../data';
 
 const List = props => {
-
-    return (null);
+    
+    return (
+        <div className="list">
+        {
+            team.map(item => {
+                let fname = item.fname;
+                let lname = item.lname;
+                let role = item.role;
+                let email = item.email;
+                return(
+                <div className="card">
+                    <h3>{fname} {lname}</h3>
+                    <p>Role: {role}</p>
+                    <p>Email Address: {email}</p>
+                </div>
+                )
+            })
+        }
+        </div>
+    );
 }
 
 export default List;
